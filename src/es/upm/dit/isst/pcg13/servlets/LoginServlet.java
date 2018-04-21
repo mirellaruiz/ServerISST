d9a768de47d41ba9cee976ee404d9170dd6a9c59
Diego <diego2.gd@gmail.com>
2018-04-22 00:39:52 +0200

    Contacto Servlet

--- src/es/upm/dit/isst/pcg13/servlets/LoginServlet.java
+++ src/es/upm/dit/isst/pcg13/servlets/LoginServlet.java
@@ -1,7 +1,9 @@
 package es.upm.dit.isst.pcg13.servlets;
 
 import java.io.IOException;
-import java.io.Serializable;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,9 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
 import es.upm.dit.isst.pcg13.dao.UserDAOImplementation;
 import es.upm.dit.isst.pcg13.dao.model.User;
-import com.google.gson.*;
 
 
 @WebServlet("/LoginServlet")
@@ -24,8 +27,11 @@ protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws Se
 	String password = req.getParameter("password");
 	System.out.println(nick+" "+password);
 	User user = null;
-	//persistimos el dato
+	
+	//Persistimos el dato
+	
 	user = UserDAOImplementation.getInstance().loginUser(nick, password);
+	
 	resp.setContentType("application/json");
 	resp.setCharacterEncoding("utf-8");
 	String json;
@@ -36,6 +42,17 @@ protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws Se
 	json =new  Gson().toJson("ok");	
 	}
 	resp.getWriter().write(json);
+	
+	/* ESTO ES UNA PRUEBA DE OTRA FORMA DE MANDAR UN JSON
+	 List<String> lista = new ArrayList<String>();
+	 lista.add(nick+"hola");
+	 lista.add(password);
+	 
+	 Gson gson= new Gson();
+	 PrintWriter pw=resp.getWriter();
+	 pw.println( gson.toJson(lista));
+	 pw.close();
+	 */
 	
 	
 	
